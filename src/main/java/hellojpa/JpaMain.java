796c85b0485a218fa943b2bd2295a801dc96b161
DeveloperRooney <han_0427@naver.com>
2022-02-22 12:21:01 +0900

    준영속성에 대한 이해

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,35 +4,32 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
+
         tx.begin();
 
         try {
+            Member member1 = em.find(Member.class, 200L);
 
-            // 비영속 상태
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
 
-            // 영속 상태
-           em.persist(member);
+            Member member2 = em.find(Member.class, 200L);
 
+
+            System.out.println("======= Before Commit =======");
             tx.commit();
-        } catch(Exception e) {
+        }catch (Exception e) {
             tx.rollback();
-        } finally {
+        }finally {
             em.close();
         }
+
         emf.close();
 
 
