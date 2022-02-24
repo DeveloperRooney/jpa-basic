57b042a045a6f871f8487c0ab49837c46055c4ab
DeveloperRooney <han_0427@naver.com>
2022-02-24 12:55:05 +0900

    JPA 매핑을 통한 양방향 연관 관계 학습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -22,16 +23,25 @@ public class JpaMain {
             team.setName("TeamA");
             em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("Member1");
-            member.setTeam(team);
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("Member1");
+            member1.setTeam(team);
+            em.persist(member1);
 
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
+            Member member2 = new Member();
+            member2.setUsername("Member2");
+            member2.setTeam(team);
+            em.persist(member2);
 
-            System.out.println("findMember = " + findMember.getUsername());
-            System.out.println("findTeam = " + findMember.getTeam().getName());
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member1.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("Member = " + m.getUsername());
+            }
 
             tx.commit();
         }catch (Exception e) {
